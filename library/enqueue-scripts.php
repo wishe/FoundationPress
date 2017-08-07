4dc433e4787d5291192f62d04837dda451996149
Alex <alex.uleberg@gmail.com>
2017-08-07 13:04:12 +0200

    Update enqueue-scripts.php
    Wrong file path function used in filemtime

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -14,14 +14,14 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 
 	// Enqueue the main Stylesheet.
 	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/dist/assets/css/app.css', array(), 
-									 filemtime(get_template_directory_uri() . '/dist/assets/css/app.css'), 'all' );
+									 filemtime(get_template_directory() . '/dist/assets/css/app.css'), 'all' );
 
 	// Deregister the jquery version bundled with WordPress, as we're injecting the version we need with Webpack.
 	wp_deregister_script( 'jquery' );
 
 	// Enqueue Founation scripts
 	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/dist/assets/js/app.js', array(), 
-										filemtime(get_template_directory_uri() . '/dist/assets/js/app.js'), true );
+										filemtime(get_template_directory() . '/dist/assets/js/app.js'), true );
 
 	// Enqueue FontAwesome from CDN. Uncomment the line below if you don't need FontAwesome.
 	wp_enqueue_script( 'jquery', 'https://use.fontawesome.com/5016a31c8c.js', array(), '4.7.0', true );
