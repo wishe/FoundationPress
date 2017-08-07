9e37e8d1424ae98ab01843f9497be0a2065438a1
Alex <alex.uleberg@gmail.com>
2017-08-07 11:10:07 +0200

    Update enqueue-scripts.php
    Added filemtime to assets for cache bust

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -13,13 +13,15 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
 
 	// Enqueue the main Stylesheet.
-	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/dist/assets/css/app.css', array(), '2.10.1', 'all' );
+	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/dist/assets/css/app.css', array(), 
+									 filemtime(get_template_directory_uri() . '/dist/assets/css/app.css'), 'all' );
 
 	// Deregister the jquery version bundled with WordPress, as we're injecting the version we need with Webpack.
 	wp_deregister_script( 'jquery' );
 
 	// Enqueue Founation scripts
-	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/dist/assets/js/app.js', array(), '2.10.1', true );
+	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/dist/assets/js/app.js', array(), 
+										filemtime(get_template_directory_uri() . '/dist/assets/js/app.js'), true );
 
 	// Enqueue FontAwesome from CDN. Uncomment the line below if you don't need FontAwesome.
 	wp_enqueue_script( 'jquery', 'https://use.fontawesome.com/5016a31c8c.js', array(), '4.7.0', true );
